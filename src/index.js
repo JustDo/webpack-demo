9affae1232450e2e73447269896a8e051966aff3
BruceGavin <bruce_gavin@163.com>
2019-03-31 23:29:29 +0800

    按需加载/懒加载

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+import _ from 'lodash'
+
+
 function component() {
-    return import ( /* webpackChunkName:"lodash" */ 'lodash').then(_ => {
-        var element = document.createElement('div');
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-        return element;
-    }).catch(error => 'An error occured while loading the component');
+    var element = document.createElement('div');
+    var button = document.createElement('button');
+    var br = document.createElement('br');
+
+    button.innerHTML = "Click me and look at the console!";
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+        var print = module.default;
+        print();
+    })
+
+    return element;
 }
 
-getComponent().then(component => {
-    document.body.appendChild(component);
-})
+document.body.appendChild(component())
