57c5c4b04edc905c0a6b33211bd935bc3bf1a5a1
BruceGavin <bruce_gavin@163.com>
2019-03-31 23:13:30 +0800

    动态导入 import

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import _ from 'lodash';
-
-
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-
-
-    element.appendChild(btn);
-
-    return element;
+    return import ( /* webpackChunkName:"lodash" */ 'lodash').then(_ => {
+        var element = document.createElement('div');
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        return element;
+    }).catch(error => 'An error occured while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+    document.body.appendChild(component);
+})
