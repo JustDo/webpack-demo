e91e471fa07ab01ec41a3154957b7e1ebaba7163
BruceGavin <bruce_gavin@163.com>
2019-03-31 21:06:40 +0800

    模块热替换

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-
+const webpackDevServer = require('webpack-dev-server');
 const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
-app.use(webpackDevMiddleware(compiler,{
-    publicPath:config.output.publicPath
+
+const options = {
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost'
+}
+
+webpackDevServer.addDevServerEntrypoints(config, options);
+const server = new webpackDevServer(compiler, options);
+
+server.listen(5000,'localhost',()=>{
+    console.log('dev server listening on port5000');
+})
+
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
 }));
 
-app.listen(3000,function(){
+app.listen(3000, function() {
     console.log('Example app listening on port 3000');
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
     },
     devtool: 'inline-source-map',
-    devServer: {
-        contentBase: "./dist"
-    },
+
     plugins: [
         new CleanWebpackPlugin(), //清理dist
         new HtmlWebpackPlugin({ //在dist 目录生成html,当entry 修改或添加名称时，用它重新生成html, 将新名称引入
-            title: 'Output Management'
-        })
+            title: 'Hot Module Replacement'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename: '[name].bundle.js',
